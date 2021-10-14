9277026e4336b966b556fd0fb5546912cafbfa58
Jaenil Parekh <jaenilparekh@Jaenils-MacBook-Air.local>
2021-10-14 13:52:59 +0530

    basic functional project created

--- src/App.js
+++ src/App.js
@@ -1,30 +1,83 @@
 import React,{useEffect ,useState} from 'react'
 
 const SpeechRecognition =  window.webkitSpeechRecognition || window.SpeechRecognition ;
+const mic = new SpeechRecognition();
+
+mic.continous = true ;
+mic.onresult = console.log ;
+mic.lang = 'en-US' ;
+
+
+//Additional Date feature
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
 
 const App = () => {
   const [start, setStart] = useState(false) ;
+  const [reply, setReply] = useState('')
+  //const dictionary = useState([])
   
-  /*useEffect(() => {
-    const recognition = new SpeechRecognition();
-    recognition.onresult = console.log ;
-    recognition.start() ;
-  }, []) */
+  useEffect(() => {
+   handleFunction() ;
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [start]) 
+  
+  const handleFunction = () => {
+    if (start === false) {
+      mic.stop() ;
+      console.log('mic has stopped')
+    } else{ 
+      mic.start() ;
+    }
+
+    mic.onresult = (event) => {
+      setReply(event.results[0][0].transcript) 
+     // dictionary.unshift(reply) 
+      //setReply('')
+      console.log(reply)
+    }
+
+    mic.onstart = () =>{
+      console.log('mic has started now')
+    }
+
+    mic.onnomatch = () => {
+      setReply('No possible matches found ')
+    }
+
+    mic.onerror = event => {
+      console.log(event.error) 
+    }
+  }
 
   const toggleFunction = (state) => {
-    setStart(!state) ;
+    setStart(!state) ; 
   }
   return (
     <>
       <h1>Speech to Text application</h1>
       <hr></hr>
       <div>
-        <button>Start/Stop</button>
-        <p>results here</p>
-
+        <button onClick={() => toggleFunction(start)}>Start/Stop</button>
+        <div>
+         {reply}
+         {/*
+          <ul>
+          {
+            dictionary.map(item =>(
+              <li> {item} {console.log(item)} </li>
+            ))
+          }
+          </ul>
+          */} 
+         
+        </div>
       </div> 
       <hr></hr>
-      <p>Built by Jaenil Parekh</p>
+      <p className='footer_'>Built by Jaenil Parekh</p>
+      <div>
+         {date}
+      </div>
     </>
   )
 }
